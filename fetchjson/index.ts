7f7f07c073c9351445d66a29e7cc226433c8107c
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-08-01 15:45:39 +0200

    adding interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,19 +3,17 @@
 import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/17';
 
-interface Todo {
+interface Todoo {
   id: number;
-  userId: number;
   title: string;
   completed: boolean;
 
 }
 
 axios.get(url).then(response => {
-  const todo = response.data;
-  const id = todo.Id;
-  const userId = todo.userId;
-  const title = todo.Title;
+  const todo = response.data as Todoo;
+  const id = todo.id;
+  const title = todo.title;
   const complete = todo.completed;
   console.log(`the id is ${id} the userID is ${userId} the title is ${title} the complete is ${complete}`);
 })
