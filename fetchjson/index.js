10a8f7b4840f89740836ccb3030eac99175a02c6
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-08-01 15:14:31 +0200

    adding progress

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -1,8 +1,12 @@
 "use strict";
-/** @format */
 exports.__esModule = true;
 var axios_1 = require("axios");
-var url = 'https://jsonplaceholder.typicode.com/todos/1';
+var url = 'https://jsonplaceholder.typicode.com/todos/17';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var id = todo.id;
+    var userId = todo.userId;
+    var title = todo.title;
+    var complete = todo.completed;
+    console.log("the id is ".concat(id, " the userID is ").concat(userId, " the title is ").concat(title, " the complete is ").concat(complete));
 });
--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,8 +1,21 @@
-/** @format */
+
 
 import axios from 'axios';
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+const url = 'https://jsonplaceholder.typicode.com/todos/17';
+
+interface Todo {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
+
+}
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+  const id = todo.Id;
+  const userId = todo.userId;
+  const title = todo.Title;
+  const complete = todo.completed;
+  console.log(`the id is ${id} the userID is ${userId} the title is ${title} the complete is ${complete}`);
 })
