a4c2ef8c0cfe2904ba09ffe4b807c7cc888a40e7
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-08-02 13:54:28 +0200

    adding some type test

---
+++ typesExamples/annotations/variables.ts
@@ -0,0 +1,23 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+// Classes
+class Car {}
+let car: Car = new Car();
+// Object literal
+let point: {y:number; x:number; s:string} = {
+  x: 10,
+  s: "hello",
+  y: 20,
+  
+}
---
+++ typesExamples/types.ts
@@ -0,0 +1,12 @@
+const today = new Date();
+console.log(today.toUTCString());
+
+
+const person = {
+  age: 20,
+  name: 'John',
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
+
+}
+
