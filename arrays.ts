a99776c39c16bb60e11bac8fc632a6bf14b13590
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-08-05 13:45:30 +0200

    add

---
+++ arrays.ts
@@ -0,0 +1,12 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carByMake = [
+  ['f150'],
+  ['corolla'],
+  ['camaro'],
+]
+
+carMakers.map((car: string):string =>{
+return car;
+})
