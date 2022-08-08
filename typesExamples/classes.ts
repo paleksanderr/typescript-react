d6ce4601b904524b82f8a9371f65e14829879863
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-08-08 09:02:06 +0200

    add

--- typesExamples/classes.ts
+++ typesExamples/classes.ts
@@ -1,20 +1,34 @@
 class Vehicle {
-  public drive():void {
-    console.log("driving");
-  }
-  public honk(): void {
+  constructor(public age: number,public color: string) {
+  } 
+
+  protected honk(): void {
     console.log("beep");
   }
 }
 
+const vehicle = new Vehicle(10, 'siwy');
+console.log(vehicle.age);
+console.log(vehicle.color);
+
+
 class Car extends Vehicle {
+  constructor(public wheels: number, age:number,  color: string) {
+    super(age, color);
+  }
 
 private drive(): void {
   console.log("vroom");
 }
+startDrivingProcess():void {
+  this.drive();
+  this.honk();
+  console.log(car.wheels);
+  console.log(car.color);
+}
 
 }
 
-const car = new Car();
-car.drive();
-car.honk();
+const car = new Car(2, 14, 'blue');
+car.startDrivingProcess();
+
