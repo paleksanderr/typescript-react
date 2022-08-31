5503fd6dcefffe7453313f365dca30215f50b7eb
Paweł Aleksander <pawel.aleksander@gmail.com>
2022-08-31 13:37:49 +0200

    add to do list in ts react

--- tsr/src/props/Child.tsx
+++ tsr/src/props/Child.tsx
@@ -1,4 +1,24 @@
+interface ChildProps{
+  color: string;
+  onClick: () => void;
+  children?: React.ReactNode;
+}
 
-export const Child = () => {
-  return <div>Hi there!</div>;
+
+export const Child = ({ color, onClick }: ChildProps) => {
+  return <div>
+    {color}
+    <button onClick={onClick}>Click Me</button>
+    </div>;
 };
+
+export const ChildFc: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+		<div>
+			{color}
+			<button onClick={onClick}>Click Me</button>
+		</div>
+  );
+}
+
+
--- tsr/src/props/Parent.tsx
+++ tsr/src/props/Parent.tsx
@@ -2,7 +2,9 @@ import { Child } from './Child';
 
 
 const Parent = () => {
-  return <Child />;
+  return <Child color='red' onClick={() => console.log('clicked')} >
+    ?
+    </Child>;
 }
 
 export default Parent;
